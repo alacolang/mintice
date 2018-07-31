8539868a1eb28f120ffe73c3a6ed1399b36f125b
Yasser <yassermzh@gmail.com>
2018-07-31 09:44:00 +0430

    Revert "try animations on page navigation"
    This reverts commit 2d43a03bfe874f46434fb4b53236f58991aecf7e.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,7 @@
 // @flow
-import * as React from "react";
-import { StyleSheet, Text, View, Animated } from "react-native";
-import {
-  NativeRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-  withRouter
-} from "react-router-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NativeRouter as Router, Route, Redirect } from "react-router-native";
 import localize from "../utils/localize";
 import MyText from "./MyText";
 import Profile from "./Profile";
@@ -20,115 +14,20 @@ import Breathing from "./Breathing";
 import About from "./About";
 import { codePushify } from "../utils/codepush";
 
-type AnimatedChildProps = {
-  children: React.Node,
-  // anim: Animated.Value,
-  atParent: boolean,
-  animating: boolean
-};
-
-class AnimatedChild extends React.Component<AnimatedChildProps, any> {
-  state = {
-    previousChildren: null,
-    anim: new Animated.Value(0)
-  };
-  componentWillReceiveProps(nextProps) {
-    console.log("componentWillReceiveProps> nextProps:", nextProps);
-    const navigatingToParent = nextProps.atParent != this.props.atParent;
-    const animationEnded = this.props.animating && !nextProps.animating;
-
-    if (navigatingToParent) {
-      console.log("animating...", Date.now());
-      this.setState(
-        {
-          previousChildren: this.props.children
-        },
-        () => {
-          Animated.timing(this.state.anim, {
-            toValue: 1,
-            duration: 1000
-          }).start(this.enter);
-        }
-      );
-    } else if (animationEnded) {
-      this.enter();
-    }
-  }
-  enter = () => {
-    console.log("enter called", Date.now());
-    this.setState(
-      {
-        previousChildren: null
-      },
-      () => {
-        Animated.timing(this.state.anim, {
-          toValue: 0,
-          duration: 1000
-        }).start();
-      }
-    );
-  };
-  render() {
-    const { children } = this.props;
-    const { previousChildren } = this.state;
-    console.log("children=", children);
-    console.log("previousChildren=", previousChildren);
-    // return children;
-    return (
-      <Animated.View
-        style={{
-          // flex: 1,
-          position: "absolute",
-          left: 0,
-          right: 0,
-          top: this.state.anim.interpolate({
-            inputRange: [0, 1],
-            outputRange: [20, 0]
-          })
-          // opacity: this.state.anim.interpolate({
-          //   inputRange: [0, 0.75],
-          //   outputRange: [0, 1]
-          // })
-        }}
-      >
-        {previousChildren || children}
-        {/* {children} */}
-      </Animated.View>
-    );
-  }
-}
-
-const RouterChildren = props => (
-  <View style={styles.container}>
-    <Route path="/" component={Init} />
-    <AnimatedChild atParent={props.location.key} animating={true}>
-      <Switch location={props.location}>
-        {/* <View style={styles.container}> */}
-        <Route path="/" exact render={() => <Redirect to={routes.game} />} />
-        <Route path={routes.profile} component={Profile} />
-        <Route path={routes.breathing} component={Breathing} />
-        <Route path={routes.game} component={Game} />
-        <Route path={routes.report} component={Report} />
-        <Route path={routes.about} component={About} />
-        {/* </View> */}
-      </Switch>
-    </AnimatedChild>
-  </View>
+const App = () => (
+  <Router>
+    <View style={styles.container}>
+      <Route path="/" component={Init} />
+      <Route path="/" exact render={() => <Redirect to={routes.breathing} />} />
+      <Route path={routes.profile} component={Profile} />
+      {/*<Route path={routes.gameFeedback} component={GameFeedback} />*/}
+      <Route path={routes.breathing} component={Breathing} />
+      <Route path={routes.game} component={Game} />
+      <Route path={routes.report} component={Report} />
+      <Route path={routes.about} component={About} />
+    </View>
+  </Router>
 );
-const RouterChildrenWithRouter = withRouter(RouterChildren);
-
-class App extends React.Component<any, any> {
-  state = {
-    animating: true
-  };
-  render() {
-    return (
-      <Router>
-        <RouterChildrenWithRouter />
-      </Router>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
