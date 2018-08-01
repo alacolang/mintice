ac23881206680f7fcfa5f023e582c1e89fb77690
Yasser <yassermzh@gmail.com>
2018-08-02 00:27:52 +0430

    reuse progress bar in breathing

--- src/Components/BlockProgressBar.js
+++ src/Components/BlockProgressBar.js
@@ -1,47 +1,21 @@
 // @flow
-import React from "react";
-import {connect} from "react-redux";
-import {length} from "ramda";
-import {View, StyleSheet} from "react-native";
-import type {State as RootState} from "../logic/reducers";
+import { connect } from "react-redux";
+import { length } from "ramda";
+import ProgressBar from "./ProgressBar";
+import type { State as RootState } from "../logic/reducers";
 import config from "../config";
 
-type Props = {
-  progress: number
-};
-const defaultProps = {
-  progress: 0
-};
-
-const BlockProgressBar = ({progress} = defaultProps) => (
-  <View style={styles.container}>
-    <View style={[styles.bar, {width: `${progress}%`}]} />
-  </View>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    width: "100%",
-    alignItems: "flex-end"
-  },
-  bar: {
-    height: 12,
-    backgroundColor: "lightgreen"
-  }
-});
-
 const mapStateToProps = (state: RootState) => {
-  const {metrics: {blockID}, blocks} = state.game;
-  if (!blockID) return {progress: 0};
+  const {
+    metrics: { blockID },
+    blocks,
+  } = state.game;
+  if (!blockID) return { progress: 0 };
   const block = blocks[blockID];
   return {
     progress: Math.floor(
-      (length(block.trialIDs) - 1) / config.blockTrials * 100
-    )
+      ((length(block.trialIDs) - 1) / config.blockTrials) * 100
+    ),
   };
 };
-export default connect(mapStateToProps)(BlockProgressBar);
+export default connect(mapStateToProps)(ProgressBar);
---
+++ src/Components/ProgressBar.js
@@ -0,0 +1,34 @@
+// @flow
+import React from "react";
+import { Platform } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+type Props = {
+  progress: number,
+};
+const defaultProps: Props = {
+  progress: 0,
+};
+
+const ProgressBar = ({ progress }: Props = defaultProps) => (
+  <View style={styles.container}>
+    <View style={[styles.bar, { width: `${progress}%` }]} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    top: Platform.OS === "ios" ? 20 : 0,
+    left: 0,
+    right: 0,
+    width: "100%",
+    alignItems: "flex-end",
+  },
+  bar: {
+    height: 12,
+    backgroundColor: "#5a5f67",
+  },
+});
+
+export default ProgressBar;
